4799749087ce184965bf3c990754e420d1eac79c
SEEK-THE-VOID-LIGHT <ian55139819@gmail.com>
2020-07-29 22:58:16 +0200

    small changes: --> toggle features --> other things

--- builder.py
+++ builder.py
@@ -1,45 +1,92 @@
   
 import sys
-if len(sys.argv) < 2:
-    if sys.argv[2] != int or sys.argv[1] != int:
-        print("Usage: editor.py <number of rows> <number of collums>")
-        sys.exit("Pls use my program right :(")
-else:
-    import pygame
+# try:
+#     if sys.argv[1] > 6 and sys.argv[1] < 31 and sys.argv[2] > 6 and sys.argv[2] < 31:
+#         import pygame
+#     else:
+#         print("Use a range from 6 to 30 grid pieces")
+#         sys.exit()
+# except:
+#     print("ann error")
+#     sys.exit()
+import pygame
 
-ROWS = int(sys.argv[1])
-COLLUMS = int(sys.argv[2])
+
+ROWS = int(sys.argv[2])
+COLLUMS = int(sys.argv[1])
 RECTBASE = 30
-win = pygame.display.set_mode((COLLUMS*RECTBASE, ROWS*RECTBASE + 90))
+win = pygame.display.set_mode((COLLUMS*RECTBASE, ROWS*RECTBASE + 100))
 run = True
 icon = pygame.image.load("icon.png")
 pygame.display.set_icon(icon)
 pygame.display.set_caption("Level builder")
 clock = pygame.time.Clock()
 
+
+# <<<GRID CLASS>>> #
 class Grid(pygame.sprite.Sprite):
     def __init__(self,x,y):
         super().__init__()
         self.image = pygame.Surface((RECTBASE,RECTBASE))
-        self.color = (255,0,255)
+        self.color = (255,255,255)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-
+        self.toggled = False
     def collision(self, mouse_x, mouse_y):
         if mouse_x > self.rect.x and mouse_x < self.rect.x+self.rect.width:
             if mouse_y < self.rect.y+self.rect.width and mouse_y > self.rect.y:
                 return True
+    def toggle(self, togglecolor):
+        if self.toggled:
+            self.color = (255,255,255)
+            self.toggled = False
+        else:
+            self.color = togglecolor
+            self.toggled = True
+
+
+# <<<BUTTON CLASS>>> #
+class Button(pygame.sprite.Sprite):
+    def __init__(self,x,y,color):
+        super().__init__()
+        self.base = 40
+        self.image = pygame.Surface((self.base,self.base))
+        self.color = color
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+        self.sprite = pygame.sprite.RenderPlain(self)
+        self.toggled = False
+    def draw(self,win):
+        self.sprite.update()
+        self.sprite.draw(win)
+        if self.toggled:
+            pygame.draw.rect(win, (0,100,255), (self.rect.x,self.rect.y,self.base,self.base), 3)
+        else:
+            pygame.draw.rect(win, (0,0,0), (self.rect.x,self.rect.y,self.base,self.base), 3)
+    def collision(self,mouse_x,mouse_y):
+        if mouse_x > self.rect.x and mouse_x < self.rect.x+self.rect.width:
+            if mouse_y < self.rect.y+self.rect.width and mouse_y > self.rect.y:
+                return True
+    def toggle(self,buttons):
+        for button in buttons:
+            button.toggled = False
+        self.toggled = True
+        return self.color        
+
 
 # >>> REDRAW <<< #
-def redraw(grid):
+def redraw(grid,buttons):
     win.fill((255,255,255))
     for g in grid:
         pygame.draw.rect(win, g.color, (g.rect.x,g.rect.y,RECTBASE,RECTBASE))
         pygame.draw.rect(win, (0,0,0), (g.rect.x,g.rect.y,RECTBASE,RECTBASE), 1)
+    for button in buttons:
+        button.draw(win)
     pygame.display.update()
 
-#create grid
+#create grid and other classes
 gx = 0
 gy = 0
 grid = []
@@ -49,7 +96,10 @@ for i in range(COLLUMS):
         gy += RECTBASE
     gy = 0
     gx += RECTBASE
-    
+
+buttons = [Button(10, ROWS * RECTBASE + 10,(255,0,0)),Button(50, ROWS * RECTBASE + 10,(255,255,0)),Button(90, ROWS * RECTBASE + 10,(0,255,0)),Button(130, ROWS * RECTBASE + 10,(0,255,255)),Button(10, ROWS * RECTBASE + 50,(0,0,255)),Button(50, ROWS * RECTBASE + 50,(255,0,255)),Button(90, ROWS * RECTBASE + 50,(0,0,0))]
+
+togglecolor = (0,0,0)
 while run:
     clock.tick(100)
     for event in pygame.event.get():
@@ -59,6 +109,10 @@ while run:
             mouse_x,mouse_y = pygame.mouse.get_pos()
             for g in grid:
                 if g.collision(mouse_x,mouse_y):
-                    g.color = (0,255,255)
+                    g.toggle(togglecolor)
                     print(g.rect.topleft)
-    redraw(grid)
+            for button in buttons:
+                if button.collision(mouse_x,mouse_y):
+                    togglecolor = button.toggle(buttons)
+
+    redraw(grid,buttons)
